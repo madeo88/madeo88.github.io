b8ebc9549bf5251e66b4103335640abdf9bcc594
madeo88 <138253855+madeo88@users.noreply.github.com>
2023-07-03 23:08:11 +0200

    new modify first version of site

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function generateQRCode() {
+var qrCodes = [];
+
+function generateQRCode(event) {
+    event.preventDefault();
+
     var nickname = document.getElementById('nickname').value;
     var instagramUrl = "https://www.instagram.com/" + nickname + "/";
-    var qrCodeContainer = document.getElementById('qrCodeContainer');
-    qrCodeContainer.innerHTML = '';
+    var qrCodeTable = document.getElementById('qrCodeTable');
+    var qrCodeRow = document.createElement('tr');
+    var qrCodeCell = document.createElement('td');
+    var qrCodeContainer = document.createElement('div');
 
     var qrCode = new QRCode(qrCodeContainer, {
         text: instagramUrl,
@@ -10,10 +16,26 @@ function generateQRCode() {
         height: 200
     });
 
+    qrCodeContainer.className = 'qrCodeContainer';
+    qrCodeCell.appendChild(qrCodeContainer);
+    qrCodeRow.appendChild(qrCodeCell);
+    qrCodeTable.appendChild(qrCodeRow);
+
+    qrCodes.push(qrCodeContainer);
+    positionQRCodeContainers();
+
     var qrCodeLink = document.createElement('a');
     qrCodeLink.href = instagramUrl;
     qrCodeLink.target = "_blank";
     qrCodeLink.innerHTML = "Apri profilo Instagram";
 
     qrCodeContainer.appendChild(qrCodeLink);
+}
+
+function positionQRCodeContainers() {
+    for (var i = 0; i < qrCodes.length; i++) {
+        var qrCodeContainer = qrCodes[i];
+        qrCodeContainer.style.top = (i * 210) + "px";
+        qrCodeContainer.style.left = (i * 210) + "px";
+    }
 }
