5c0cc3c5a964a1fe13bfed1c57aa4f9d6c31c1b9
madeo88 <138253855+madeo88@users.noreply.github.com>
2023-07-03 22:09:37 +0200

    deleted files

--- generate_qr.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['nickname'])) {
-    $nickname = $_GET['nickname'];
-    $instagramUrl = "https://www.instagram.com/$nickname/";
-    $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode($instagramUrl) . "&size=200x200";
-    echo '<img src="' . $qrCodeUrl . '">';
-}
-?>
