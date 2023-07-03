c488de63c6a536d72d071cfce75ddf128cfe1c20
madeo88 <138253855+madeo88@users.noreply.github.com>
2023-07-03 21:46:52 +0200

    Update generate_qr.php

--- generate_qr.php
+++ generate_qr.php
@@ -2,7 +2,8 @@
 if (isset($_POST['nickname'])) {
     $nickname = $_POST['nickname'];
     $instagramUrl = "https://www.instagram.com/$nickname/";
-    $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode($instagramUrl) . "&size=200x200";
-    echo '<img src="' . $qrCodeUrl . '">';
+	echo $instagramUrl;
+	echo urlencode($instagramUrl);
+    
 }
 ?>
