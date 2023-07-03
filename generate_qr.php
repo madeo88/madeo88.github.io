ecb973cbb6c807e5370f7685a2340bb21d40f513
madeo88 <138253855+madeo88@users.noreply.github.com>
2023-07-03 21:51:19 +0200

    modify request method

--- generate_qr.php
+++ generate_qr.php
@@ -1,9 +1,8 @@
 <?php
-if (isset($_POST['nickname'])) {
-    $nickname = $_POST['nickname'];
+if (isset($_GET['nickname'])) {
+    $nickname = $_GET['nickname'];
     $instagramUrl = "https://www.instagram.com/$nickname/";
-	echo $instagramUrl;
-	echo urlencode($instagramUrl);
-    
+    $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode($instagramUrl) . "&size=200x200";
+    echo '<img src="' . $qrCodeUrl . '">';
 }
 ?>
